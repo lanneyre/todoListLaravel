7798a5333ace621c5a601e5696fb0aec6d6b4b51
LANNEY Remi <r.lanney@ics-nice.com>
2021-02-26 10:13:54 +0100

    Projet V1

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,58 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="panel-body">
+        @include('common.errors')
+
+        <form action="{{url('tasks')}}" method="post" class="form-horizontal">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">Task</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" id="task" name="name">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Task
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        @if (count($tasks)>0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($tasks as $task)
+                        <tr>
+                            <td class="table-text">
+                                <div>{{$task->name}}</div>
+                            </td>
+                            <td>
+                                <form action="{{ url('tasks/'.$task->id) }}" method="post">
+                                    {{ csrf_field() }}
+                                    {!! method_field('DELETE') !!}
+                                    <button class="btn btn-danger">
+                                        <i class="fa fa-trash"></i> Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        @endif
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Tasks;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['middleware' => 'web'], function (){
+
+    Route::get('/', function () {
+        $tasks = Tasks::orderBy('created_at', 'asc')->get();
+        // var_dump($tasks);
+        return view('tasks', ["tasks" => $tasks]);
+    });
+
+    Route::post('/tasks', function (Request $request) {
+        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if($validator->fails()){
+            return redirect('/')->withInput();
+        }
+
+        $task = new Tasks();
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    Route::delete('/tasks/{task}', function (Tasks $task) {
+        $task->delete();
+        return redirect('/');
+    });
+
+
+
+});
